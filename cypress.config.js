9fb0bdee49dcf44fd9ef712bd774a251d8d051d9
LatikaBalla <latika.balla@kellton.com>
2024-04-03 22:30:13 +0530

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
-
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   projectId: 'qt2s1p',
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
+            return config;
       // implement node event listeners here
     },
   },
